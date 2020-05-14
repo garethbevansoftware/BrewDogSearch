dd79fdcbbb923ba30cee9e3bef8287cc2892782b
Gareth Bevan <garethbevansoftware@gmail.com>
2020-05-14 12:20:12 +0100

    Updates

---
+++ precache-manifest.90d2b26d8f0691f3c81b5cec035e1dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1304168a0458ddf066950329e2cebe4b",
+    "url": "/BrewDogSearch/index.html"
+  },
+  {
+    "revision": "bb5c5cee268700b46ca9",
+    "url": "/BrewDogSearch/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5be78ac74b36f7fb3594",
+    "url": "/BrewDogSearch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb5c5cee268700b46ca9",
+    "url": "/BrewDogSearch/static/js/2.21583b93.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/BrewDogSearch/static/js/2.21583b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be78ac74b36f7fb3594",
+    "url": "/BrewDogSearch/static/js/main.c0ca0724.chunk.js"
+  },
+  {
+    "revision": "11de537270e112fb74aa",
+    "url": "/BrewDogSearch/static/js/runtime-main.892a82a6.js"
+  }
+]);
